dc3ce9942d03f983287f5834b761ee0a14d4a584
Daniel Salgado <88348530+Salgaado@users.noreply.github.com>
2022-06-04 20:52:14 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,24 @@
+const people = [  
+    {  
+     img:  
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959121/person-1_aufeoq.jpg",  
+     name: "Kalleb",  
+     job: "product manager",  
+     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis? `,  
+    },  
+    {  
+     img:  
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-2_ipcjws.jpg",  
+     name: "Daniella",  
+     job: "developer",  
+     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,  
+    },  
+    {  
+     img:  
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/c_scale,w_200/v1595959131/person-3_rxtqvi.jpg",  
+     name: "Yasmim",  
+     job: "designer",  
+     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,  
+    },  
+   ];  
+   export default people;  
